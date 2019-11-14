8a10893d30a0465487aef47f8b259ed40d3208f8
Sreekumar Athiparambath <sreekumaralan@gmail.com>
2019-11-14 10:37:54 +0530

    added bottumnavigationbar

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -11,23 +11,27 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
-        backgroundColor: Colors.blue,
+        leading: Icon(Icons.navigate_before),
         middle: Text('Home'),
-        leading: GestureDetector(
-          child: Icon(Icons.navigate_before),
-          onTap: () {},
-        ),
+        trailing: Icon(Icons.search),
+        backgroundColor: Colors.blue,
       ),
-      child: CupertinoActionSheet(
-        title: Text('Choose'),
-        message: Text('your option'),
-        actions: <Widget>[
-          CupertinoActionSheetAction(
-            child: Text('hi'),
-            onPressed: () {},
-          )
-        ],
-      ),
+      child: CupertinoTabScaffold(
+          tabBar: CupertinoTabBar(items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.phone), title: Text('Phone')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.chat_bubble), title: Text('Chat')),
+          ]),
+          tabBuilder: (context, int i) {
+            return Center(
+              child: Text('$i'),
+            );
+          }),
     );
   }
+
+  Widget childSomething() => Center(
+        child: Text('something'),
+      );
 }
